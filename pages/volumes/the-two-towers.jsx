0c1444d7a61a2fac8e4543b80e1a9d6e396f4f1a
Denes <csaszar.denes@gmail.com>
2023-04-21 15:15:01 +0200

    lord-of-the-rings-ready

---
+++ pages/volumes/the-two-towers.jsx
@@ -0,0 +1,24 @@
+import { volumes } from "../../lib/data";
+import Link from "next/link";
+import Image from "next/image";
+
+export default function VolumePage2() {
+  const volume = volumes.find(({ slug }) => slug === "the-two-towers");
+  return (
+    <div>
+      <Link href="/">All Volumes</Link>
+      <h1>{volume.title}</h1>
+      <p>{volume.description}</p>
+      <ul>
+        {volume.books.map((book) => (
+          <li key={book.slug}>
+            {book.ordinal} - {book.title}
+          </li>
+        ))}
+      </ul>
+      <Image src={volume.cover} width={140} height={230} alt="image" />
+      <Link href="/volumes/the-fellowship-of-the-ring">Previous Volume</Link>
+      <Link href="/volumes/the-return-of-the-king">Next Volume</Link>
+    </div>
+  );
+}
